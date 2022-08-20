f320a086f0a96ec6bbb3ec2568b5aeef56d2a535
Oussama Manar <omanar@e2r13p11.1337.ma>
2022-08-20 04:49:23 +0100

    add default directory

--- script.sh
+++ script.sh
@@ -27,30 +27,31 @@ echo "Insert Project title:" # Ask for project title
 read projectName # Get project title
 
 # Define auxiliary variables
-headerfile="$projectName/includes/$projectName.h"
-mainfile="$projectName/srcs/$projectName.c"
+DFL_DIR="$HOME/Desktop";
+headerfile="$DFL_DIR/$projectName/includes/$projectName.h"
+mainfile="$DFL_DIR/$projectName/srcs/$projectName.c"
 upper=$(echo "$projectName" | awk '{print toupper($0)}')
 
-if [ -z $projectName ]; then # If project title is empty
+if [ -z $DFL_DIR/$projectName ]; then # If project title is empty
 	echo -e "\033[0;31m </ -- Enter The Project Title! -- >\n\033[0m" # Display error message
-elif [ -d $projectName ]; then # If project title is already exist
+elif [ -d $DFL_DIR/$projectName ]; then # If project title is already exist
 	echo -e "\033[0;31m\n </ -- Project Title Already Exists! -- >\n\033[0m" # Display error message
 else # If project title is valid
-	mkdir $projectName # Create project directory
-	mkdir $projectName/includes # Create includes directory
+	mkdir $DFL_DIR/$projectName # Create project directory
+	mkdir $DFL_DIR/$projectName/includes # Create includes directory
 	touch $headerfile # Create header file
-	mkdir $projectName/srcs # Create srcs directory
+	mkdir $DFL_DIR/$projectName/srcs # Create srcs directory
 	touch $mainfile # Create main file
 	echo "#include <$projectName.h>" >> $mainfile # Include header file in main file
 	cat >> $mainfile < templates/main.template # Add main file template
-	mkdir $projectName/libs # Create libs directory
+	mkdir $DFL_DIR/$projectName/libs # Create libs directory
 	echo "Add libft? (y/n):" # Ask for libft choice
 	read answer # Get libft choice
 	if [ -z $answer ] || ( [ $answer != "y" ] && [ $answer != "n" ] ); then # If libft choice is empty or invalid
 		echo -e "\033[31m\n </ -- An error occurred in the answer -- >\033[0m" # Display error message
 		echo -e "\033[33m\n </ -- Libft Not Added -- >\033[0m" # Display message
 	elif [ $answer = "y" ]; then # If libft choice is yes
-		cp -r templates/libft $projectName/libs/libft # Add libft directory
+		cp -r templates/libft $DFL_DIR/$projectName/libs/libft # Add libft directory
 		echo -e "\033[0;36m\n </ --  Libft Added Successfully --  >\033[0m" # Display success message
 	else # If libft choice is no
 		echo -e "\033[33m\n </ -- Libft Not Added -- >\033[0m" # Display message
@@ -58,7 +59,7 @@ else # If project title is valid
 	echo "#ifndef "$upper"_H" >> $headerfile # Define header file
 	echo "# define "$upper"_H" >> $headerfile # Define header file
 	echo "" >> $headerfile # Add epmty line
-	if [ -d $projectName/libs/libft ]; then # If libft directory is exist
+	if [ -d $DFL_DIR/$projectName/libs/libft ]; then # If libft directory is exist
 		echo "# include <libft.h>" >> $headerfile # Add libft header file
 	fi # End if libft directory is exist
 	echo "# include <stdio.h>" >> $headerfile # Add stdio header file
@@ -66,15 +67,15 @@ else # If project title is valid
 	echo "# include <stdlib.h>" >> $headerfile # Add stdlib header file
 	echo "" >> $headerfile # Add epmty line
 	echo "#endif" >> $headerfile # End define header file
-	touch $projectName/Makefile # Create Makefile
-	echo "NAME	=	$projectName" >> $projectName/Makefile # Add Makefile template
-	cat >> $projectName/Makefile < templates/Makefile.template # Add Makefile template
+	touch $DFL_DIR/$projectName/Makefile # Create Makefile
+	echo "NAME	=	$projectName" >> $DFL_DIR/$projectName/Makefile # Add Makefile template
+	cat >> $DFL_DIR/$projectName/Makefile < templates/Makefile.template # Add Makefile template
 	echo -e "\033[1;32m\n </ $projectName Has Been Created Successfully! >\n\033[0m" # Display success message
 fi # End if project title is valid
 
 echo -e	"\n    	 Please report any issues:"
 echo -en "\n\033[1;30m    	     GitHub"
 echo -en "\033[1;32m ➜  \033[0m"
-echo -e	"\033[1;35momanar42\033[0m"
+echo -e	"\033[1;35momanar42\033\n[0m"
 
 # ==================== END ==================== #
